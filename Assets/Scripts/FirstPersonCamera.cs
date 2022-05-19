7ba4a5cf55fe9e51b34eead92b8d5b4b91d414a0
Jack <54145187+ChaoticNutritional@users.noreply.github.com>
2022-05-19 19:40:34 -0400

    Camera Controller

--- Assets/Scripts/FirstPersonCamera.cs
+++ Assets/Scripts/FirstPersonCamera.cs
@@ -1,9 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class FirstPersonCamera : MonoBehaviour
 {
+    //Camera attached to the player
+    [Tooltip("The Camera attached to the player")]
+    public Camera playerCam;
+
+
     // Start is called before the first frame update
     void Start()
     {
